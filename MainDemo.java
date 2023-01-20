07ac9e6365f2b78ac179b9c8fc2337556b2909e3
iamtushar007 <81921024+iamtushar007@users.noreply.github.com>
2023-01-20 19:36:54 +0530

    MainDemo

---
+++ MainDemo.java
@@ -0,0 +1,12 @@
+public class MainDemo {
+
+    // Main Overloading
+    public static void main(String[] args) {
+        System.out.println("Main...");
+        main();
+    }
+
+    public static void main() {
+        System.out.println("Main with no args");
+    }
+}
