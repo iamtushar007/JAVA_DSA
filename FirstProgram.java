eee66b5aa3a97e22ae513545f9289308a81aea40
iamtushar007 <81921024+iamtushar007@users.noreply.github.com>
2023-01-20 19:11:57 +0530

    First Program

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+class FirstProgram {
+    static public void main(String Args[]) {
+        System.out.println("Hello world");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("* * * * * * * * * * *");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+    }
+}
