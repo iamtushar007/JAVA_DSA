471fda86c3dc956b8c891745c1b477ccd75a18b3
iamtushar007 <81921024+iamtushar007@users.noreply.github.com>
2023-01-20 19:12:24 +0530

    Java Basics

---
+++ JavaBasics.java
@@ -0,0 +1,25 @@
+public class JavaBasics {
+
+    public static void main(String[] args) {
+        int id;
+        id = 1001;
+        byte age = 22;
+        byte rollNo = (byte) 130;
+        // Byte has a range of -128 to 127
+        System.out.println(rollNo);
+        float roi = 9.7F;
+        // float roi = 9.7f;
+        // float roi = (float) 9.7;
+        double amount = 10000.2345;
+        boolean att = true;
+        char termAndConditions = 'Y';
+        char value = 'a';
+        System.out.println(value);
+        System.out.println(att);
+        String name = "Tushar";
+        String name2 = name;
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name == name2);
+    }
+}
